08586363f8b1b6ce04780e059a34e30f2e23cef7
AuroraFO <159686134+AuroraFO@users.noreply.github.com>
2024-08-15 12:52:41 +0800

    Add files via upload

--- src/consts.ts
+++ src/consts.ts
@@ -2,13 +2,13 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_TAB = "Frosti";
-export const SITE_TITLE = "Frosti 🧊";
+export const SITE_TAB = "AuroraFO";
+export const SITE_TITLE = "AuroraFO";
 export const SITE_DESCRIPTION = "A blog template";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "EveSunMaple";
+export const USER_NAME = "AuroraFO";
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
@@ -31,32 +31,32 @@ export const infoTest = {
 
 // Menu items for navigation
 export const menuItems = [
-  { id: "home", text: "Home", href: "/", svg: "home", target: "_self" }, // Home page
-  { id: "about", text: "About", href: "/about", svg: "about", target: "_self" }, // About page
+  { id: "home", text: "首页", href: "/", svg: "home", target: "_self" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "about", target: "_self" }, // About page
   {
     id: "blog",
-    text: "Blogs",
+    text: "博客",
     href: "/blog",
     svg: "blog",
     target: "_self",
     subItems: [
       {
         id: "all",
-        text: "All blogs",
+        text: "所有博客",
         href: "/blog",
         svg: "post",
         target: "_self",
       }, // All blog
       {
         id: "tech",
-        text: "Tech blogs",
+        text: "科技博客",
         href: "/blog/categories/tech",
         svg: "cube",
         target: "_self",
       }, // Technology category
       {
         id: "life",
-        text: "Life blogs",
+        text: "生活博客",
         href: "/blog/categories/life",
         svg: "heart",
         target: "_self",
@@ -65,22 +65,22 @@ export const menuItems = [
   }, // Blog page with sub-items
   {
     id: "project",
-    text: "Project",
+    text: "项目",
     href: "/project",
     svg: "project",
     target: "_self",
   }, // Projects page
   {
     id: "friend",
-    text: "Friend",
+    text: "朋友",
     href: "/friend",
     svg: "friend",
     target: "_self",
   }, // Friends page
   {
     id: "contact",
-    text: "Contact",
-    href: "mailto:contact.evesunmaple@outlook.com", // Contact email
+    text: "联系",
+    href: "mailto:contact.3795199897@qq.com", // Contact email
     target: "_blank", // Open in a new tab
     svg: "contact",
   },
@@ -95,13 +95,13 @@ export const socialIcons = [
     svg: "support",
   },
   {
-    href: "https://github.com/EveSunMaple",
+    href: "https://github.com/AuroraFO",
     ariaLabel: "Github",
     title: "Github",
     svg: "github",
   },
   {
-    href: "https://space.bilibili.com/438392347",
+    href: "https://space.bilibili.com/1670180008",
     ariaLabel: "BiliBili",
     title: "BiliBili",
     svg: "bilibili",
